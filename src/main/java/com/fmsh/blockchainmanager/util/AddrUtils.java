d38c3546a55bfec15c1664054948b1329f18ef8c
yuanjiaxin <yuanjiaxin@fmsh.com.cn>
2018-07-05 16:39:33 +0800

    初始化

---
+++ src/main/java/com/fmsh/blockchainmanager/util/AddrUtils.java
@@ -0,0 +1,30 @@
+package com.fmsh.blockchainmanager.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AddrUtils {
+
+    public static String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+    public static int getPortAddress(HttpServletRequest request) {
+        return request.getRemotePort();
+    }
+}
